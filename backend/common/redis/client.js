4763824c2da5f029675e165ffb491266a5b53a1e
elavarasan <elavarasan@lemonpay.tech>
2026-05-27 13:05:34 +0530

    added sentainel.config file first we have to run sentainel server using redis-sentinel sentinel.conf comment then run redis server 1) redis-server --port 6379 then second replica redis-server --port 6380 --replicaof 127.0.0.1 6379 .

--- backend/common/redis/client.js
+++ backend/common/redis/client.js
@@ -1,9 +1,87 @@
-const Redis = require('ioredis');
+// const Redis = require('ioredis');
 
-// const redis = new Redis("redis://redis:6379");
-const redis = new Redis();
+// // const redis = new Redis("redis://redis:6379");
+// const redis = new Redis();
 
-redis.on('connect', () => console.log('Redis connected'));
-redis.on('error', (err) => console.error('Redis error', err));
+// redis.on('connect', () => console.log('Redis connected'));
+// redis.on('error', (err) => console.error('Redis error', err));
 
-module.exports = redis;
+// module.exports = redis;
+
+/////////////////////////// normal redis test code without sentailnes ///////
+// const Redis = require("ioredis");
+
+// const redis = new Redis();
+
+// redis.on("connect", () => {
+//   console.log("Redis Connected");
+// });
+
+// redis.on("error", (err) => {
+//   console.log("Redis Error");
+// });
+
+// setInterval(async () => {
+//   try {
+//     const time = new Date().toISOString();
+
+//     await redis.set("time", time);
+
+//     console.log("Stored:", time);
+//   } catch (err) {
+//     console.log("FAILED");
+//   }
+// }, 1000);
+
+// module.exports = redis;
+/////////////////////////// normal redis test code without sentailnes ///////
+
+const Redis = require("ioredis");
+
+const redis = new Redis({
+  sentinels: [
+    {
+      host: "127.0.0.1",
+      port: 26379,
+    },
+  ],
+  name: "mymaster",
+});
+
+async function logCurrentNode() {
+  try {
+    const info = await redis.info("server");
+
+    const port = info.split("\n").find((line) => line.startsWith("tcp_port"));
+
+    console.log("Connected Redis:", port);
+  } catch (err) {
+    console.log("Cannot fetch node info");
+  }
+}
+
+redis.on("connect", () => {
+  console.log("Redis Connected");
+
+  logCurrentNode();
+});
+
+redis.on("reconnecting", () => {
+  console.log("Reconnecting to Redis...");
+});
+
+redis.on("error", (err) => {
+  console.log("Redis Error");
+});
+
+setInterval(async () => {
+  try {
+    await redis.set("time", Date.now());
+
+    console.log("Stored");
+  } catch (err) {
+    console.log("FAILED");
+  }
+}, 1000);
+
+setInterval(logCurrentNode, 5000);
